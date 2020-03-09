b7497fd36298257282644545ff740c60a501ae26
kjason1998 <kjason1998@gmail.com>
2020-03-09 01:19:41 +0000

    Member functions and member page made

--- Screen/Member.js
+++ Screen/Member.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, FlatList, StatusBar } from 'react-native';
 
 import { Container, Content, Header, Form, Input, Item, Button, Left, Right, Body, Icon, Title } from 'native-base';
+import { ScrollView } from 'react-native-gesture-handler';
+import { membershipBenefits } from '../constans/StringsMembership'
 
 const Member = props => {
 
@@ -23,20 +25,34 @@ const Member = props => {
                     </Button>
                 </Right>
             </Header>
-            <View>
-
-            </View>
+            <ScrollView>
+                <View style={styles.containerMember}>
+                    <View style={styles.backgroundContainer}>
+                        <Image source={require('../Image/profile_bg_default.png')} style={styles.imageBackground} />
+                    </View>
+                    <View style={styles.profileView}>
+                        <Image style={styles.logo} source={require('../Image/profile_default.png')} />
+                    </View>
+                </View>
+                <View style={styles.contentView}>
+                    <Text style={styles.membershipText}>Membership</Text>
+                    <Text style={styles.premiumText}>Premium</Text>
+                    <Text style={styles.HeadingDescription}>All members receive the following membership benefits:​</Text>
+                    <Text style={styles.descriptionText}>{membershipBenefits}</Text>
+                </View>
+            </ScrollView>
         </Container>
     );
 }
 
 const win = Dimensions.get('window');
-const ratioWidth = win.width / 649 / 2;
+const ratioWidth = win.width / 375;
+const heightBg = 257 * ratioWidth;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width:win.width,
+        width: win.width,
     },
     iconMenu: {
         color: (Platform.OS === 'ios') ? '#147efb' : '#fff'
@@ -48,6 +64,45 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 18,
         color: (Platform.OS === 'ios') ? '#000' : '#fff',
+    }, backgroundContainer: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    profileView: {
+        // opacity: 0.5,
+        // backgroundColor: '#000000'
+    },
+    logo: {
+        marginTop: heightBg / 2.5,
+    },
+    imageBackground: {
+        resizeMode: 'cover',
+        height: heightBg,
+        width: win.width,
+    },
+    containerMember: {
+        alignItems: 'center',
+    },
+    contentView: {
+        marginVertical: 15,
+        width: win.width,
+        flexDirection: 'column',
+        alignItems: 'center'
+    }, membershipText: {
+        fontSize: 24,
+    }, premiumText: {
+        fontSize: 24,
+        color: '#01758E',
+        fontStyle: 'italic',
+        fontWeight:'bold'
+    },descriptionText:{
+        margin:20
+    },HeadingDescription:{
+        marginTop:20,
+        fontSize:18
     }
 });
 
---
+++ constans/StringsMembership.js
@@ -0,0 +1,8 @@
+export const membershipBenefits = 
+    "\u25cf A posted, paper copy of the AISBQ (Quarterly) magazine, which contains concise articles covering the latest research in AI, book reviews, conference reports, opinion pieces, and Father Hacker, in the UK and beyond."+
+    "\n\n\u25cf Weekly e-mail bulletin, which contains the latest news, conference calls, job opportunities and information on worldwide AI events."+
+    "\n\n\u25cf Reduced registration fees at the annual AISB convention, one of the UK’s biggest general AI conferences. Typically this reduced rate makes it more affordable to join the Society and then register for the convention than to attend the convention as a non-member."+
+    "\n\n\u25cf Reduced registration fees at the ECAI conferences through AISB’s membership of EurAI and all other EurAI benefits including free access to EurAI AI Communications from the Members Only Area."+
+    "\n\n\u25cf Travel awards from both AISB and EurAI (priority given to student members)."+
+    "\n\n\u25cf Assistance in the formation of local and regional AI Groups through small grants towards recruiting external speakers."+
+    "\n\n\u25cf Electronic subscription to Connection Science."
